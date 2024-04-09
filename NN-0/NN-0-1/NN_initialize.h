7c30b631631198e36191bff2adeb9a46e28a52fb
U-SIMONS-PLAYHOUS\15037 <wesselsimon00@gmail.com>
2024-04-09 15:40:23 -0700

    basic

--- NN-0/NN-0-1/NN_initialize.h
+++ NN-0/NN-0-1/NN_initialize.h
@@ -9,7 +9,7 @@ class NN_initialize {
     std::vector<double>* create_FirstLayer(int qty);
     std::vector<double>* create_SecondLayer(int qty);
     std::vector<double>* create_OutputLayer(int qty);
-    std::vector<double>* create_WeightsToOneNeuron(int recieving);
+    //std::vector<double>* create_WeightsToOneNeuron(int recieving);
     
     std::vector<std::vector<double>>* create_WeightsOneLayerMatrix(int incoming_layers, int current_layers);
     void set_MatrixOneLayer_ToRandom(std::vector<std::vector<double>>* MatrixPtr);
