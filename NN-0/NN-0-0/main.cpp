7b54c82c5f380e49c15b8186f92110ed9fd05cb4
U-SIMONS-PLAYHOUS\15037 <wesselsimon00@gmail.com>
2024-04-08 17:17:02 -0700

    made version 0-1

--- NN-0/NN-0-0/main.cpp
+++ NN-0/NN-0-0/main.cpp
@@ -1,1 +1,31 @@
-test to see if this works from laptop!
+#include <iostream>
+#include <vector>
+
+// basic neural network from scratch
+
+class perceptron{
+  public:
+    // create each layer with a function
+    void make_InputLayer(int qty) {
+    std::vector<double>* inputPtr = new std::vector<double>(qty);
+    return (inputPtr);
+    }
+
+    void make_FirstLayer(int qty) {
+      std::vector<double>* FirstLayerPtr = new std::vector<double>(qty);
+      return (FirstLayerPtr);
+    }
+    void make_SecondLayer(int qty){
+      std::vector<double>* SecondLayerPtr = new std::vector<double>(qty);
+      return (SecondLayerPtr);
+    }
+    void make_OutputLayer(int qty){
+      std::vector<double>* OutputLayerPtr = new std::vector<double>(qty);
+      return (OutputLayerPtr)
+    }
+}
+
+
+int main () {
+  return 0;
+}
---
+++ NN-0/NN-0-1/initialize.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+class NN_initialize {
+  public:
+    
+    //these functions should allow the user to easily initialize 4 empty vectors where neuron activations can be stored
+
+    std::vector<double>* create_InputLayer (int qty) {
+      std::vector<double>* InputLayerPtr = new std::vector<double>(qty);
+      return InputLayerPtr;
+    }
+
+    std::vector<double>* create_FirstLayer (int qty) {
+      std::vector<double>* FirstLayerPtr = new std::vector<double>(qty);
+      return FirstLayerPtr;
+    }
+
+    std::vector<double>* create_SecondLayer (int qty) {
+      std::vector<double>* SecondLayerPtr = new std::vector<double>(qty);
+      return SecondLayerPtr;
+    }
+
+    std::vector<double>* create_OutputLayer (int qty) {
+      std::vector<double>* OutputLayerPtr = new std::vector<double>(qty);
+      return OutputLayerPtr;
+    }
+
+
+
+    // these functions are supposed to help the user create the weights for each neuron and link them up
+    std::vector<double>* create_WeightsToOneNeuron (int recieving) {
+      std::vector<double>* WeightsToOneNeuronPtr = new std::vector<double>(recieving);
+      return WeightsToOneNeuronPtr;
+    // I would really like to store all the variables for each neuron in one vector or maybe in one matrix or an array of vectors
+    //  -> it would be really nice if I i could somehow link each neuron to the weights and one bias that belongs to it ... this might prove nearly impossible though and could slow down the program speed
+    }
+    
+
+    // call all functions to initialize the empty neural network
+    void ..... // this might not actually work ... unless I return an array?
+}
+
+
+
+
+
+
+
+
+int main () {
+
+  return 0;
+}
