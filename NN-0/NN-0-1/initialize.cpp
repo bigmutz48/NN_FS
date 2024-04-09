11e6706a4ce0b4140ad68b9cfc53bdfb5edfeac4
U-SIMONS-PLAYHOUS\15037 <wesselsimon00@gmail.com>
2024-04-09 08:17:10 -0700

    OMG INITIALIZE.CPP COMPILED git add .git add . INIT-0-0.exe IS NOW AVAILABLEgit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .!

--- NN-0/NN-0-1/initialize.cpp
+++ NN-0/NN-0-1/initialize.cpp
@@ -1,3 +1,5 @@
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 
@@ -48,8 +50,28 @@ class NN_initialize {
       std::vector<std::vector<double>>* WeightsOneLayerMatrixPtr = new std::vector<std::vector<double>>(current_layer , std::vector<double>(incoming_layer));
       return WeightsOneLayerMatrixPtr;
     }
+    void set_MatrixOneLayer_ToRandom (std::vector<std::vector<double>>* MatrixPtr){
+      
+      // set up the random number generator
+      srand(static_cast<unsigned int>(time(nullptr)));
 
-}
+      // return the amount of columns of the matrix
+      int num_vectors = MatrixPtr->size();
+      
+      // return the amount of rows of the matrix
+      int vector_size = MatrixPtr->at(0).size();
+
+
+      // run the nested loops to randomize all the values
+      for (int i = 0; i <= num_vectors ; ++i ) {
+        for (int j = 0; i<= vector_size; ++i){
+          // to each entry Matrix[i][j] ... assign a random value between -1 and 1
+          (*MatrixPtr)[i][j] = static_cast<double>(rand()) / RAND_MAX * 2.0 - 1.0;
+        }
+      }
+    }
+
+};
 
 
 int main () {
@@ -66,21 +88,29 @@ int main () {
 
 
   // create each layer using Ptrs!!!!
-  InputLayerPtr = NN.create_InputLayer(Input);
-  FirstLayerPtr = NN.create_FirstLayer(First);
-  SecondLayerPtr = NN.create_SecondLayer(Second);
-  OutputLayerPtr = NN.create_OutputLayer(Output);
+  std::vector<double>* InputLayerPtr = NN.create_InputLayer(Input);
+  std::vector<double>* FirstLayerPtr = NN.create_FirstLayer(First);
+  std::vector<double>* SecondLayerPtr = NN.create_SecondLayer(Second);
+  std::vector<double>* OutputLayerPtr = NN.create_OutputLayer(Output);
   
   // create the weights for each layer!!!!
   
   // input -> first layer
-  WeightsFirstLayerMatrixPtr = NN.create_WeightsOneLayerMatrix(Input, First);
+  std::vector<std::vector<double>>* WeightsFirstLayerMatrixPtr = NN.create_WeightsOneLayerMatrix(Input, First);
 
   // first layer -> second layer
-  WeightsSecondLayerMatrixPtr = NN.create_WeightsOneLayerMatrix(First, Second);
+  std::vector<std::vector<double>>* WeightsSecondLayerMatrixPtr = NN.create_WeightsOneLayerMatrix(First, Second);
 
   // second layer -> output 
-  WeightsOutputLayerMatrixPtr = NN.create_WeightsOneLayerMatrix(Second, Output);
+  std::vector<std::vector<double>>* WeightsOutputLayerMatrixPtr = NN.create_WeightsOneLayerMatrix(Second, Output);
+
+
+  // randomize all weight matrices
+  
+  //has to be done for each matrix
+  NN.set_MatrixOneLayer_ToRandom(WeightsFirstLayerMatrixPtr);
+  NN.set_MatrixOneLayer_ToRandom(WeightsSecondLayerMatrixPtr);
+  NN.set_MatrixOneLayer_ToRandom(WeightsOutputLayerMatrixPtr);
 
 
   return 0;
