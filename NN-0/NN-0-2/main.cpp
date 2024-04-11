5b6c130cf8a675831b67171d7c0eb7dcabf3d7ec
U-SIMONS-PLAYHOUS\15037 <wesselsimon00@gmail.com>
2024-04-11 15:35:07 -0700

    fidgeting

--- NN-0/NN-0-2/main.cpp
+++ NN-0/NN-0-2/main.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 #include <fstream>
 
-initialize_NN.createNuersons 
-WriteToFile.createNeurons
 // 1) set up the empty neurons with pointers
 // 2) set up the bias and weight vectors with random values
 class initialize_NN{
@@ -15,7 +13,13 @@ class initialize_NN{
     }
     
     std::vector<double>* create_BiasesOneLayer(int qty_CurrentLayer){
-        std::vector<double>* BiasVectorPtr = new std::vector<double>(qty_CurrentLayer);
+        std::vector<double>* BiasVectorPtr = new std::vector<double>(qty_CurrentLayer); 
+        
+         srand(static_cast<unsigned int>(time(nullptr)));// seed the random num generator
+
+        for (int i = 0; i < BiasVectorPtr->size()){ // set each bias to a random value when the system is first created
+          (*BiasVectorPtr)[i] = static_cast<double>(rand()) / RAND_MAX; 
+        }
         return BiasVectorPtr;
     }
 
@@ -31,7 +35,7 @@ class initialize_NN{
       for (int i = 0 ; i < WeightsMatrixPtr->size(); i++){
         for (int j = 0; j < WeightsMatrixPtr->at(0).size(); j++){ // set each value in WeightsMatrix to be a random 
                                                                   // number between 0 and 1
-          (*WeightsMatrixPtr)[i][j] = static_cast<double>(rand()) / RAND_MAX * 2.0 - 1.0 ;  // might be better to do this 
+          (*WeightsMatrixPtr)[i][j] = static_cast<double>(rand()) / RAND_MAX ;  // might be better to do this 
                                                                                             // using % mod function
         }
       }
@@ -50,7 +54,27 @@ class WriteToFile{
 
 // 1) run forward propagation for given weights, bias, and input neuron activations
 class run_ForwardPropagation{
-  public:
+  public:  
+    double ReLU(double raw_input){
+    return ans;
+    }
+    void write_ActivationsNextLayer(int IncomingLayerINDEX,
+                                    int CurrentLayerNumberINDEX,
+                                    std::vector<std::vector<double>>* WeightsMatrixPtr,
+                                    std::vector<double>* BiasVectorPtr,
+                                    
+                                    ){
+
+   // calculate the activations of each of the current layer neurons 
+    for (int i = 0 ; i < qty_CurrentLayer ; i++){ // for each nueron in the current layer
+
+      
+    }
+
+
+
+
+    }
 };
 
 
