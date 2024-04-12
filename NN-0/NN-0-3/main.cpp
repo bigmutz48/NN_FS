74dd14bab2e79fc1c48a2ec045e1612e0174228d
U-SIMONS-PLAYHOUS\15037 <wesselsimon00@gmail.com>
2024-04-12 16:11:27 -0700

    version 0.3 drafted

--- NN-0/NN-0-3/main.cpp
+++ NN-0/NN-0-3/main.cpp
@@ -7,15 +7,48 @@ class initialize_NN{
   public:
     
     std::vector<double> create_BiasesOneLayer(int qty_CurrentLayer){
+      std::vector<double> BiasLayer(qty_CurrentLayer);
+      
 
+      // set all to random numbers between 0 and 1  
+      // seed random generator
+      
+      srand(static_cast<unsigned int>(time(nullptr)));
+
+      // set each value in the vector to be random 
+      for (int i = 0 ; i < qty_CurrentLayer ; i++){
+        BiasLayer[i] = static_cast<double>(rand()) / RAND_MAX;
+      }
+
+      return BiasLayer;
     }
     
-    std::vector<std::vector<double>> create_WeightsMatrixBetweenTwoLayers(int qty_CurrentLayer){
+    std::vector<std::vector<double>> create_WeightsMatrixBetweenTwoLayers(int qty_CurrentLayer,
+                                                                          int qty_IncomingLayer){
+      std::vector<std::vector<double>> LayerWeights(qty_CurrentLayer, std::vector<double>(qty_IncomingLayer));
       
+
+
+      // need to set each value in this matrix to a random value between one and zero 
+      
+      // seed random num generator
+      srand(static_cast<unsigned int>(time(nullptr)));
+
+      
+
+      // set each value in the matrix to be a random value between 1 and 0
+      for (int i = 0 ; i < qty_CurrentLayer ; i++){
+        for (int j = 0 ; j < qty_IncomingLayer ; j++){
+          LayerWeights[i][j] = static_cast<double>(rand()) / RAND_MAX;
+        }
+      }
+
+      return LayerWeights;
     }
 
     std::vector<double> create_NeuronsOneLayer(int qty_CurrentLayer){
-
+      std::vector<double> NeuronLayer(qty_CurrentLayer);
+      return NeuronLayer;
     }
 
 
@@ -30,7 +63,16 @@ class initialize_NN{
                                                                                 int qty_Second,
                                                                                 int qty_Third,
                                                                                 int qty_Output){
-      
+      std::vector<std::vector<std::vector<double>>> AllLayersWeightMatrices(4);
+
+      AllLayersWeightMatrices[0] = create_WeightsMatrixBetweenTwoLayers(qty_Input, qty_First); // weights matrix between input and first layer 
+      AllLayersWeightMatrices[1] = create_WeightsMatrixBetweenTwoLayers(qty_First, qty_Second); // weight matrix between first and second layers 
+      AllLayersWeightMatrices[2] = create_WeightsMatrixBetweenTwoLayers(qty_Second, qty_Third); // weight matrix between second and third layers 
+      AllLayersWeightMatrices[3] = create_WeightsMatrixBetweenTwoLayers(qty_Third, qty_Output); // weight matrix between third and output layers
+    
+
+
+      return AllLayersWeightMatrices;
     }
     
     // create the matrix that will store the activations that are read for each neuron in each layer.
@@ -39,8 +81,17 @@ class initialize_NN{
                                                                                 int qty_Second,
                                                                                 int qty_Third,
                                                                                 int qty_Output){
+    
+      std::vector<std::vector<double>> NeuronLayersMatrix(5);
 
+      NeuronLayersMatrix[0] = std::vector<double>(qty_Input);
+      NeuronLayersMatrix[1] = std::vector<double>(qty_First);
+      NeuronLayersMatrix[2] = std::vector<double>(qty_Second);
+      NeuronLayersMatrix[3] = std::vector<double>(qty_Third);
+      NeuronLayersMatrix[4] = std::vector<double>(qty_Output);
+      
 
+      return NeuronLayersMatrix;
     }
     // create the matrix that will contain all biases.
     std::vector<std::vector<double>> create_BiasesAllLayersMatrix(              int qty_Input,
@@ -48,8 +99,15 @@ class initialize_NN{
                                                                                 int qty_Second,
                                                                                 int qty_Third,
                                                                                 int qty_Output){
+      std::vector<std::vector<double>> BiasesMatrix(5);
 
+      BiasesMatrix[0] = std::vector<double>(qty_Input);
+      BiasesMatrix[1] = std::vector<double>(qty_First);
+      BiasesMatrix[2] = std::vector<double>(qty_Second);
+      BiasesMatrix[3] = std::vector<double>(qty_Third);
+      BiasesMatrix[4] = std::vector<double>(qty_Output);
 
+      return BiasesMatrix;
     }
     
 
