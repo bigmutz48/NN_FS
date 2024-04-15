d9d73da3d9d77735760ca9c793de2fbd6726c60a
U-ACADEMICWEAPONB\wesse <wesselsimon00@gmail.com>
2024-04-15 15:36:10 -0700

    going to try to make a super simple network that takes only a minimal amount of inputs before trying to tackle something bigger

--- SUPERBASIC_FS/SB-0/SB-0-0/main.cpp
+++ SUPERBASIC_FS/SB-0/SB-0-0/main.cpp
@@ -9,12 +9,24 @@
 
 class CreateData{
   public:
+};
+
+
+class CreateNetwork{
+  public:
+};
+
+
+class WriteToFile{
+  public:
 }
 
 
-
-
-
 int main(){
+  CreateNetwork INIT;
+  CreateData DATA;
+  WriteToFile WRITE;
+
+
   return 0;
 }
