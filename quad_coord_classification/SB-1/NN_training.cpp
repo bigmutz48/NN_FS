8efd84b0316c28067c36ccab390e97872f8b2de2
U-ACADEMICWEAPONB\wesse <wesselsimon00@gmail.com>
2024-05-08 13:07:41 -0700

    looks like i might be ready to try and compile

--- quad_coord_classification/SB-1/NN_training.cpp
+++ quad_coord_classification/SB-1/NN_training.cpp
@@ -29,33 +29,25 @@ void Training::run_TrainingOneLayer(int layer_index){
 
   // for each neuron in the current layer 
   for (int i = 0 ; i < NeuronsPtr[layer_index].size() ; i++){
-    // for each weight for this specific neuron
-    for (int j = 0 ; j < NeuronsPtr[layer_index - 1].size() ; j++){
-      
-      //create a temporary variable to store the value of the gradient of the cost function with respect to this weight
-      double weightgradient = 0;
 
 
+
+
+//////////////////////////////////// GENERAL SETUP FOR A NEURON  //////////////////////////////////////////
+
+
+      // set dL_dA here so you don't continously loop through in calculating 
+      // every single weight ... that would be a waste of time
+      double dL_dA = 0;
+      double dA_dZ = 0;
 
       //3 things to calculate!
       //1) dL_dA -> the derivative of the loss with respect to the activation
       //2) dA_dZ -> the derivative of the activation with respect to the raw input
       //3) dZ_dW_ij -> the derivative of the raw input with respect to the specific weight we are looking at
 
-      // set these values up so we can fill them up
-      double dL_dA = 0;
-      double dA_dZ = 0;
-      double dZ_dW_ij = 0;
 
 
-
-
-      // going to run the calculations here
-
-            // first ... very easy ... dZ_dW_ij is equal to A^(n-1)_j
-            // dZ_dW_ij gets the neuron activation from the previous layer at index j
-            dZ_dW_ij = this-> NeuronsPtr[layer_index - 1][j];
-            
             // DERIVATIVE: dA_dZ 
             // this is a lot harder ... going to need to have the z value for the current layer_index at neuron j
             if (ZValuesPtr[layer_index - 1][i] <= 0){
@@ -68,11 +60,45 @@ void Training::run_TrainingOneLayer(int layer_index){
             // this one is only moderately loco
             // this one is the hardest and requires a sum function
             for (int i = 0 ; i < NeuronsPtr[layer_index].size() ; i++){
-              dL_dA += (NeuronsPtr[layer_index][i])
+              dL_dA += (NeuronsPtr[layer_index][i] - )
             }
+    
+    
+//really quickly we want to calculate the gradient for the bias of this neuron 
+//////////////////////////////////// BIASES ///////////////////////////////////////////
+
+double biasgradient = 0;
 
 
-    // calculate the gradient for this weight
+//DERIVATIVE: dZ_dBi
+//this describes the relationship between the bias and the raw input function
+dZ_dBi = 1
+
+
+
+biasgradient = dL_dA * dA_dZ * dZ_dBi; 
+
+    this->BiasesPtr[layer_index - 1][i] = biasgradient;
+
+
+
+
+
+
+
+
+
+      // We want to:
+      // 1) calculate dZ_dW_ij for the weight ij 
+      // 2) multiply dZ_dW_ij dL_dA and dA_dZ 
+      // 3) write that product to the gradient jagged array
+      // for each weight for this specific neuron
+      for (int j = 0 ; j < NeuronsPtr[layer_index - 1].size() ; j++){
+      // initialize these values equal to zero ... they will only be used in this j for loop       
+      double weightgradient = 0;
+      double dZ_dW_ij = 0;
+
+      // calculate the gradient for this weight
     weightgradient = dL_dA * dA_dZ * dZ_dW_ij;
     
     // actually do the assignment here
