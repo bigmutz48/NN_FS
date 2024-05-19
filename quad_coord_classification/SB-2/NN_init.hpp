f7166d851352daa390acddee4a07b4c64116a29f
U-ACADEMICWEAPONB\wesse <wesselsimon00@gmail.com>
2024-05-19 12:09:41 -0700

    pushing

--- quad_coord_classification/SB-2/NN_init.hpp
+++ quad_coord_classification/SB-2/NN_init.hpp
@@ -1,6 +1,10 @@
 #ifndef NN_INIT_HPP
 #define NN_INIT_HPP
 
+
+
+
+
 // This class is only for initializng a blank neural net with no parameters
 
 struct neuron{
@@ -10,16 +14,30 @@ struct neuron{
   double raw_activation_val;
   std::vector<double>* weights_vector = nullptr;
   
-}
+};
+
 
 struct NN{
+  //init parameters
+  int input =  -1;
+  int first =  -1;
+  int second = -1;
+  int third =  -1;
+  int output = -1;
+
+
+  // constructor for object
+  NN(int input, int first, int second, int third, int output);
+  // main network info
   int num_training_examples;
   std::vector<double>* input_layer = nullptr;
   std::vector<neuron>* first_hidden_layer_neurons = nullptr;
   std::vector<neuron>* second_hiddenlayer_neurons = nullptr;
   std::vector<neuron>* third_hidden_layer_neurons = nullptr;
   std::vector<neuron>* output_layer_neurons = nullptr
-}
+
+  // gradient and backprop info
+};
 
 
 class NN_INIT{
--- quad_coord_classification/SB-2/NN_training.hpp
+++ quad_coord_classification/SB-2/NN_training.hpp
@@ -1,6 +1,40 @@
 #ifndef NN_TRAINING_HPP
 #define NN_TRAINING_HPP
+#include <vector>
+#include <iostream>
+#include <memory>
 
 
+struct neuron_double_partials{
+  std::vector<double> dzi_dwi;
+
+};
+
+struct NN_gradient_and_partials{
+  NN_gradient_and_partials(NN& nn);
+  
+
+
+};
+
+
+class NN_TRAINING{
+  
+
+  std::unique_ptr<NN> network_to_train_ptr = nullptr;
+  training_data* training_data_ptr = nullptr;
+  NN_gradient_and_partials* SGD = nullptr;
+
+  
+
+  public:
+  RUN_TRAINING(NN* network_to_train_ptr,
+               training_data* training_data_ptr,
+               );
+
+  ~RUN_TRAINING();
+  
+  void run_all_training();
+};
 
 #endif
